619bf9c12d3612fe2805dede9fcbdf314a4ab9d8
Renomen <renomandev@gmail.com>
2024-08-16 05:23:41 +0300

    Update Drop.ts for ppl not dropping fist

--- src/actions/Drop.ts
+++ src/actions/Drop.ts
@@ -1,3 +1,4 @@
+
 import { Action } from "../abstracts/Action.js";
 import { Peer } from "../structures/Peer.js";
 import { BaseServer } from "../structures/BaseServer.js";
@@ -15,6 +16,13 @@ export default class extends Action {
 
   public handle(peer: Peer, action: ActionType<{ action: string; itemID: string }>): void {
     const itemID = parseInt(action.itemID);
+    
+    // Protect fist and wrench added by renomansss 
+    if (itemID === 18 || itemID === 32) {
+      peer.send(Variant.from("OnConsoleMessage", "This item cannot be dropped."));
+      return;
+    }
+    
     const item = this.base.items.metadata.items.find((v) => v.id === itemID);
 
     const peerItem = peer.data?.inventory?.items.find((v) => v.id === itemID);
